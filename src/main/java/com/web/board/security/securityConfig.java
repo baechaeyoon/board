a4becc4cb7c0d89b7bd546a03afecf1ea0dbea0e
baechaeyoon <baechaeyoon@naver.com>
2022-12-25 23:19:32 +0900

    first commit(spring security first step)

---
+++ src/main/java/com/web/board/security/securityConfig.java
@@ -0,0 +1,9 @@
+package com.web.board.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class securityConfig extends WebSecurityConfigurerAdapter {
+
+}
